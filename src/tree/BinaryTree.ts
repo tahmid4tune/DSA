86d6587c07d7d2b1f29562400a4dbe3f4a0bcbd9
Tahmid Ali <tahmid.tee10@gmail.com>
2025-08-31 21:42:53 +0600

    Comments added

--- src/tree/BinaryTree.ts
+++ src/tree/BinaryTree.ts
@@ -1,3 +1,14 @@
+/*
+Example Binary tree:
+             1
+            / \
+            2  3
+           / \
+          4   5
+*/
+/**
+ * The NODE class that represents a NODE in the Binary Tree
+ */
 class Node {
   data: number;
   left: Node | null;
@@ -13,6 +24,16 @@ class Node {
 class BinaryTree {
   root: Node | null = null;
 
+  /**
+   * Insertion method within the tree;
+   * Idea is to assign ROOT node if the tree is empty;
+   * Otherwise use a queue and insert the root as the first node
+   * Then try to insert left to right nodes
+   * If left node is empty, insert new node to the left of the current node and stop there;
+   * Otherwise, add the left node to the queue
+   * If right node is empty, insert new node to the right of the current node and stop there;
+   * Otherwise, add the right node to the queue
+   */
   insert(data: number) {
     const newNode = new Node(data);
     if (!this.root) {
@@ -35,6 +56,9 @@ class BinaryTree {
     }
   }
 
+  /**
+   * Inorder: print/output at the middle
+   */
   inorder(node: Node | null = this.root): void {
     if (!node) return;
     this.inorder(node.left);
@@ -42,6 +66,9 @@ class BinaryTree {
     this.inorder(node.right);
   }
 
+  /**
+   * Postder: print/output at the end
+   */
   postOrder(node: Node | null = this.root): void {
     if (!node) return;
     this.postOrder(node.left);
@@ -49,6 +76,9 @@ class BinaryTree {
     process.stdout.write(node.data + " ");
   }
 
+  /**
+   * Preder: print/output at the beginning
+   */
   preOrder(node: Node | null = this.root): void {
     if (!node) return;
     process.stdout.write(node.data + " ");
